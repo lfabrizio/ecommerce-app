bd03fb44dca0af929494e77a3e1ee5c64fecb7d2
Louis fabrizio <louisfabrizio.student@careerdevs.com>
2019-06-12 23:27:26 +0000

    axios

---
+++ public/script.js
@@ -0,0 +1,33 @@
+function getUserInfo() {
+ const input = document.getElementById("userName").value;
+ console.log(input)
+  axios.get("/showprofile/" + input)
+   .then(response => {
+    document.getElementById("userInfo").innerHTML = response.data
+   })
+
+}
+
+function handleSubmit() {
+ const userName = document.getElementById("user-name").value;
+ const userId = document.getElementById("user-id").value;
+ const message = document.getElementById("message").value;
+ console.log(userName, userId, message);
+  const payload = {
+   username: userName,
+   id: userId,
+   message
+ }
+ axios.post("/api/", payload)
+   .then(response => {
+     console.log(response)
+   })
+
+}
+function getAllUsers() {
+ axios.get("/getallusers/")
+   .then(response => {
+     document.getElementById("result").innerHTML = JSON.stringify(response.data)
+   })
+}
+
